0c6d7550da34a6f5c6401d9e031e9a036b97b2e8
jaeilnet <sgsgself@naver.com>
2022-04-22 18:02:32 +0900

    지도 마커 찍기 완

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,7 @@ import Map from "../components/map";
 const Home: NextPage = ({ data }: any) => {
   // console.log(data);
 
-  return (
-    <div>
-      <Map data={data.subwayStationMaster} latitude={33.45} longitude={126} />
-    </div>
-  );
+  return <Map data={data.subwayStationMaster} />;
 };
 
 export default Home;
@@ -17,12 +13,10 @@ export async function getStaticProps() {
   const SEOUL_API_KEY = process.env.NEXT_PUBLIC_SEOUL_API_KEY;
 
   const response = await fetch(
-    `http://openapi.seoul.go.kr:8088/${SEOUL_API_KEY}/json/subwayStationMaster/1/30/`
+    `http://openapi.seoul.go.kr:8088/${SEOUL_API_KEY}/json/subwayStationMaster/1/742/`
   );
 
   const data = await response.json();
-
-  console.log(data, "data");
 
   return {
     props: {
