738705a6943fbbd2541f2e7138dec1464e7a0a57
jaeilnet <sgsgself@naver.com>
2022-04-26 16:22:48 +0900

    fetch error

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -132,7 +132,8 @@ const Map: React.FC<Props> = ({ data: { row }, handleStationInfo }) => {
       });
 
       async function handleFindSubway(stationName: string) {
-        console.log(encodeURIComponent(stationName));
+        console.log(stationName, "stationName");
+        console.log(decodeURI(stationName));
         const { data } = await UseFetch(
           `http://swopenapi.seoul.go.kr/api/subway/${REAL_TIEM_APIKEY}/json/realtimeStationArrival/0/5/${encodeURIComponent(
             stationName
