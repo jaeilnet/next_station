ce1ae92380794f10febf33965d46fd3ce1ba4b78
jaeilnet <sgsgself@naver.com>
2022-04-26 15:40:58 +0900

    fetch error

--- components/useCustom/UseFetch.tsx
+++ components/useCustom/UseFetch.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const UseFetch = async (url: string) => {
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    mode: "no-cors",
+  });
 
   const data = await response.json();
 
