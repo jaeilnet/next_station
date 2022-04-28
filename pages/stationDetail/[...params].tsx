d9f3868c57a907f94adce7f289cd5c55898e0035
kimnew6 <kimnew6@gmail.com>
2022-04-28 14:10:27 +0900

    Feature: 승하차 차트 추가

--- pages/stationDetail/[...params].tsx
+++ pages/stationDetail/[...params].tsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./StationDetail.module.css";
 import { API_get } from "../../components/common/api";
 import dayjs from "dayjs";
+import BarChart from "../../components/chart/BarChart";
 
-const stationDetail = ({ incourseLast, outcourseLast }: any) => {
-  console.log(incourseLast.SearchLastTrainTimeByIDService.row);
-  console.log(outcourseLast.SearchLastTrainTimeByIDService.row);
-
+const StationDetail = ({
+  incourseLast,
+  outcourseLast,
+  stationPassengerData,
+}: any) => {
+  console.log(stationPassengerData);
+  const passengerCount = stationPassengerData.CardSubwayTime.row[0];
+  const [passengerData, setPassengerData] = useState({
+    labels: [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
+    datasets: [
+      {
+        label: ["승차 인원"],
+        data: [
+          passengerCount.FOUR_RIDE_NUM,
+          passengerCount.SIX_RIDE_NUM,
+          passengerCount.EIGHT_RIDE_NUM,
+          passengerCount.TEN_RIDE_NUM,
+          passengerCount.TWELVE_RIDE_NUM,
+          passengerCount.FOURTEEN_RIDE_NUM,
+          passengerCount.SIXTEEN_RIDE_NUM,
+          passengerCount.EIGHTEEN_RIDE_NUM,
+          passengerCount.TWENTY_RIDE_NUM,
+          passengerCount.TWENTY_TWO_RIDE_NUM,
+          passengerCount.MIDNIGHT_RIDE_NUM,
+        ],
+        backgroundColor: "blue",
+      },
+      {
+        label: ["하차 인원"],
+        data: [
+          passengerCount.FOUR_ALIGHT_NUM,
+          passengerCount.SIX_ALIGHT_NUM,
+          passengerCount.EIGHT_ALIGHT_NUM,
+          passengerCount.TEN_ALIGHT_NUM,
+          passengerCount.TWELVE_ALIGHT_NUM,
+          passengerCount.FOURTEEN_ALIGHT_NUM,
+          passengerCount.SIXTEEN_ALIGHT_NUM,
+          passengerCount.EIGHTEEN_ALIGHT_NUM,
+          passengerCount.TWENTY_ALIGHT_NUM,
+          passengerCount.TWENTY_TWO_ALIGHT_NUM,
+          passengerCount.MIDNIGHT_ALIGHT_NUM,
+        ],
+        backgroundColor: "red",
+      },
+    ],
+  });
   const reversedOutcourseLast = [
     ...outcourseLast.SearchLastTrainTimeByIDService.row,
   ].reverse();
@@ -43,11 +86,14 @@ const stationDetail = ({ incourseLast, outcourseLast }: any) => {
           })}
         </div>
       </div>
+      <div style={{ width: 375 }}>
+        <BarChart chartData={passengerData} />
+      </div>
     </>
   );
 };
 
-export default stationDetail;
+export default StationDetail;
 
 export async function getStaticProps() {
   const SEOUL_API_KEY = process.env.NEXT_PUBLIC_SEOUL_API_KEY;
@@ -57,25 +103,22 @@ export async function getStaticProps() {
     else if (todayDay === 6) return 2;
     else return 1;
   };
-  // const totalCount = () => {
-  //   const totalCountData = API_get(
-  //     `http://openAPI.seoul.go.kr:8088/${SEOUL_API_KEY}/json/SearchSTNTimeTableByIDService/1/1/0220/${theDay()}/1/`
-  //   );
-  // };
 
   const { data: incourseLast } = await API_get(
     `http://openAPI.seoul.go.kr:8088/${SEOUL_API_KEY}/json/SearchLastTrainTimeByIDService/1/5/0220/${theDay()}/1/`
   );
 
-  // const totalCount = first.SearchSTNTimeTableByIDService.list_total_count;
   const { data: outcourseLast } = await API_get(
     `http://openAPI.seoul.go.kr:8088/${SEOUL_API_KEY}/json/SearchLastTrainTimeByIDService/1/5/0220/${theDay()}/2/`
   );
-  // const { data: airPolutionInfo } = await UseFetch(
-  //   `http://openapi.seoul.go.kr:8088/${SEOUL_API_KEY}/json/airPolutionInfo/1/5/`
-  // );
 
+  const line = "2호선";
+  const { data: stationPassengerData } = await API_get(
+    encodeURI(
+      `http://openapi.seoul.go.kr:8088/${SEOUL_API_KEY}/json/CardSubwayTime/1/1/202203/2호선/선릉/`
+    )
+  );
   return {
-    props: { incourseLast, outcourseLast },
+    props: { incourseLast, outcourseLast, stationPassengerData },
   };
 }
