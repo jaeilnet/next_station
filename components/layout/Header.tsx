9ad4e192d3d79227f16000077f9d7eb4e8e932a1
kimnew6 <kimnew6@gmail.com>
2022-05-11 14:17:41 +0900

    add: 뒤로가기 버튼

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -3,16 +3,26 @@ import Link from "next/link";
 import React from "react";
 import classes from "./Header.module.css";
 import Logo from "../../public/lgo.png";
+import Arrow from "../../public/arrow.png";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
   return (
     <div className={classes.container}>
-      <div className={classes.logo}>
+      <div style={{ cursor: "pointer" }} className={classes.logo}>
         <Link href="/" passHref>
           <Image src={Logo} alt="logo" width="30" height="30" />
         </Link>
       </div>
       <div>지하철</div>
+      {router.pathname === "/stationDetail/[...params]" && (
+        <div className={classes.back}>
+          <Link href="/" passHref>
+            <Image src={Arrow} alt="arrow" width="30" height="30" />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
